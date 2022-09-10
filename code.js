46958a4344873dc6170c234dbf38c4772e580c5e
Constea <constantinarabadjiu@gmail.com>
2022-09-10 21:41:19 +0300

    Changed the id of the operator buttons Made progress on the logic of the code

--- code.js
+++ code.js
@@ -1,6 +1,64 @@
+function add(a,b){
+    return a+b
+}
 
-list=[]
+function subtract(a,b){
+    return a-b
+}
 
-x="66+22"
-y=Math.floor(x)
-console.log(y)
+function multiply(){
+    firstNumber=list2[0]
+    lastNumber=list2[list2.length-1]
+    result=firstNumber*lastNumber
+    list2[0]=result
+    list2.splice(1,2)
+    console.log(result) 
+
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function getNumbers(e){
+    
+    a=e.target.innerText 
+    console.log(a) 
+    list.push(a)
+}
+
+function operatorSelection(e){
+    counter=list2.length
+    console.log(counter)
+    
+    operator=e.target.innerText
+    fullNumber=parseInt(list.join(""))
+    nanCheck=isNaN(fullNumber)
+    console.log(nanCheck)
+    if(nanCheck===false){
+        list2.push(fullNumber)
+        list=[]
+        console.log(list2)
+    }
+    
+    if(counter===1){
+
+        multiply()
+    }
+}
+
+let list=[]
+let a=0
+
+let operator=0
+let fullNumber=0
+let counter=0
+let list2=[]
+let firstNumber=0
+const numberButtons=document.querySelectorAll("#bt")
+const operatorButtons=document.querySelectorAll("#bt2")
+
+numberButtons.forEach(button=>button.addEventListener("click",getNumbers))
+
+operatorButtons.forEach(button=>button.addEventListener("click",operatorSelection))
+
